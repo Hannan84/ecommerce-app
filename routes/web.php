7dad7f682d62b886b1d7eca5a9ed66dff434f002
hannan84 <hannansarkar84@gmail.com>
2025-06-25 12:47:05 +0600

    update user model & auth

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+Route::get('/logout', [LoginController::class, 'logout']);
