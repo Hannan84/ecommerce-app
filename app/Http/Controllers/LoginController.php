7fa2fcd577c732f7b013d2aae11bab7eb6176d52
hannan84 <hannansarkar84@gmail.com>
2025-06-25 12:48:49 +0600

    create login functionalities

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,5 +3,25 @@ namespace App\Http\Controllers;
 
 class LoginController extends Controller
 {
+ public function showLoginForm()
+ {
+  return view('login');
+ }
+ public function login(Request $request)
+ {
+  $credentials = $request->only('email', 'password');
 
+  if (! $token = auth()->attempt($credentials)) {
+   return back()->withErrors(['Invalid credentials']);
+  }
+
+  $url = 'http://127.0.0.1:8000/auth/external-login?token=' . $token;
+  return redirect($url);
+ }
+
+ public function logout()
+ {
+  auth()->logout();
+  return redirect('http://127.0.0.1:8000/logout?redirect=http://ecommerce.test/login');
+ }
 }
